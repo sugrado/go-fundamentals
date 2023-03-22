33a9dc80f58f01f084441bdd303b8a47903ca77a
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-03-22 13:34:42 +0300

    add main package and function

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "introduction/interfaces"
+
+func main() {
+	// conditionals.Demo1()
+	// loops.Demo1()
+	// loops.Demo2()
+	// loops.PrimeNumber()
+	// arrays.Demo1()
+	// arrays.Demo2()
+	// arrays.Demo3()
+	// slices.Demo1()
+	// slices.Demo2()
+	// dictionary := map[string]string{"book": "kitap", "table": "masa"}
+	// for k, v := range dictionary {
+	// 	fmt.Println(k, v)
+	// }
+	//interfaces.Demo1()
+	//defer_statement.B()
+	//defer_statement.Test()
+	//defer_statement.Demo3()
+	//error_handling.Demo1()
+	interfaces.Demo2()
+}
